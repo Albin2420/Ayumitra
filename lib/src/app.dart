7f254b1aed789b84c3b2c32534c6a719b71a878a
albinantony <albinantony2420@gmail.com>
2025-07-31 20:01:11 +0530

    intial commit with project architecture

---
+++ lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:ayurmitra/src/presentation/screens/splashscreen/splash.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+class AyurMitra extends StatelessWidget {
+  const AyurMitra({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'AyurMitra',
+      debugShowCheckedModeBanner: false,
+      builder: EasyLoading.init(),
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/src/core/url.dart
@@ -0,0 +1,3 @@
+class Url {
+  static String baseUrl1 = 'https://flutter-amr.noviindus.in/api';
+}
